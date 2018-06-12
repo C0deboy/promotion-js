63373c82a7f3444cee23b33996b7a5036a849c58
Marcin Lasota <marcin.lasota@ttpsc.pl>
2018-06-12 14:38:06 +0200

    alpha

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # promotion-plugin
+
+JavaScript plugin which I use on my [blog](https://devcave.pl/) and [site](https://jaki-jezyk-programowania.pl/) to inform about promotions related to programming.
---
+++ promotion.js
@@ -0,0 +1,156 @@
+const promotions = [
+  {
+    start: new Date('2018-06-12'),
+    end: new Date('2018-06-12'),
+    number: '3607',
+    host: 'helion.pl',
+    img: 'https://static01.helion.com.pl/helion/img/newsy/f7172657bf8c0114233be4237fe93ef4.png',
+    popup: true,
+    message: `W Helion trwa [promocja] 2za1 na książki z top100. Zobacz książki, które warto kupić.`,
+    adHeader: 'W Helion trwa [promocja 2za1]:',
+    adContent: `Na książki z top 100.<br> Zobacz książki, które warto kupić w <a href="/moja-biblioteka">mojej bibliotece</a>.`,
+  },
+  {
+    start: new Date('2018-05-15'),
+    end: new Date('2018-06-12'),
+    number: '3607',
+    host: 'videopoint.pl',
+    img: 'https://videopoint.pl/img/newsy/72fac5e23eec8e0c3450d2be521b7925.gif',
+    popup: false,
+    message: 'W videopoint trwa [promocja] 2za1 na książki z top100. Zobacz książki, które warto kupić.',
+    adHeader: 'W videopoint trwa [promocja] na videokursy.',
+    adContent: `Na ksią.`,
+  },
+];
+
+const customMessage = '';
+
+function appendLinkToMessage(message, url) {
+  return message.replace(/\[(.*?)]/, `<a href="${url}" target="_blank">$1</a>`);
+}
+
+function validContext() {
+  if(location.hostname === 'jaki-jezyk-programowania.pl') {
+    return location.pathname === '/';
+  } else if (location.hostname === 'devcave.pl') {
+    return location.pathname !== '/';
+  } else {
+    return true;
+  }
+}
+
+
+promotions.forEach((promotion, i) => {
+  if (isPromotionActive(promotion)) {
+    promotion.url = new URL(`http://${promotion.host}/page/9102Q/promocja/${promotion.number}`);
+
+    promotion.message = appendLinkToMessage(promotion.message, promotion.url);
+    promotion.adHeader = appendLinkToMessage(promotion.adHeader, promotion.url);
+
+    if (customMessage !== '') {
+      promotion.message = customMessage;
+    } else {
+      showPromotionAd(promotion, i);
+    }
+    if (promotion.popup && validContext() && localStorage.getItem('ad-closed') !== '1') {
+      createPromotionMessagePopup(promotion);
+    }
+  }
+});
+
+function isPromotionActive(promotion) {
+  const currentDate = new Date();
+
+  currentDate.setHours(0, 0, 0, 0);
+  promotion.end.setHours(0, 0, 0, 0);
+  promotion.start.setHours(0, 0, 0, 0);
+
+  return currentDate >= promotion.start && currentDate <= promotion.end;
+}
+
+function createPromotionMessagePopup(promotion) {
+  const booksBtn = document.querySelector('.books-btn');
+  if (booksBtn) {
+    const promotionPopup = document.createElement('p');
+    const closeBtn = document.createElement('button');
+    closeBtn.classList.add('close-ad');
+    closeBtn.innerHTML = '<i class="fa fa-times"></i>';
+    closeBtn.setAttribute('title', 'Nie pokazuj');
+    closeBtn.setAttribute('data-toggle', 'tooltip');
+    closeBtn.setAttribute('data-placement', 'right');
+    closeBtn.addEventListener('click', () => closeAd(promotionPopup));
+
+    promotionPopup.innerHTML = promotion.message;
+    promotionPopup.classList.add('promotion-sign', 'box-effect');
+    promotionPopup.appendChild(closeBtn);
+    document.body.appendChild(promotionPopup);
+    positionPromotionPopup(booksBtn, promotionPopup);
+    window.addEventListener('resize', () => positionPromotionPopup(booksBtn, promotionPopup));
+  }
+}
+
+function closeAd(ad) {
+  localStorage.setItem('ad-closed', '1');
+  ad.style.display = 'none';
+}
+
+function positionPromotionPopup(booksBtn, popup) {
+  const booksBtnRect = booksBtn.getBoundingClientRect();
+  const left = (booksBtnRect.left - popup.offsetWidth) + +(booksBtnRect.width / 4);
+
+  if (document.documentElement.clientWidth <= 768) {
+    popup.style.right = '25px';
+  } else {
+    popup.style.left = left + 'px';
+    popup.style.right = '';
+  }
+}
+
+function getWhenEndMessage(promotion) {
+  let to;
+  if (promotion.start.valueOf() !== promotion.end.valueOf()) {
+    const currentDate = new Date();
+    currentDate.setHours(0, 0, 0, 0);
+    if (promotion.end.valueOf() === currentDate.valueOf()) {
+      to = 'Ostatni dzień promocji!'
+    } else {
+      to = `Promocja do ${promotion.end.toLocaleDateString('pl-Pl')}`;
+    }
+  } else {
+    to = 'Promocja tylko dziś!';
+  }
+  return '(' + to + ')';
+}
+
+function cloneAd(promotionLink, i) {
+  const newPromotionLink = promotionLink.cloneNode(true);
+  promotionLink.parentElement.insertBefore(newPromotionLink, promotionLink);
+  newPromotionLink.classList.add(`link-${i}`);
+  promotionLink = document.querySelector(`.promotion.link-${i}`);
+  return promotionLink;
+}
+
+function showPromotionAd(promotion, i) {
+  let promotionLink = document.querySelector('.promotion');
+
+  if (promotionLink) {
+    if (i > 0) {
+      promotionLink = cloneAd(promotionLink, i);
+    }
+    promotionLink.style.display = 'block';
+
+    const header = promotionLink.querySelector('.promotion-header');
+    header.innerHTML = promotion.adHeader;
+
+    const promoDesc = promotionLink.querySelector('.promotion-content');
+    promoDesc.innerHTML = promotion.adContent + '<br>' + getWhenEndMessage(promotion);
+
+    const imageLink = document.createElement('a');
+    imageLink.href = promotion.url.toString();
+    const img = document.createElement('img');
+    img.src = promotion.img;
+
+    imageLink.appendChild(img);
+    promoDesc.appendChild(imageLink);
+  }
+}
