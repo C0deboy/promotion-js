99da82b63b9ba0a7f2fdeb86ca56a618226823d2
C0deboy <ellucky4@gmail.com>
2018-06-13 20:05:17 +0200

    new promotion

--- updateRepositores.bat
+++ updateRepositores.bat
@@ -7,16 +7,13 @@ If /I "%INPUT%"=="n" set branch="master" & goto Execute
 echo Incorrect input & goto Ask
 
 :Execute
-echo %branch%
-
-@echo on
 
 git add -A && git commit -m "new promotion" && git push origin %branch%
 cd ../jaki-jezyk-programowania || pause && exit
-git submodule update --remote
+git checkout promotion-js && git submodule update --remote
 git add promotion-js && git commit -m "updated promotion" && git push origin %branch%
 cd ../devcave || echo "Devcave won't be updated" && pause && exit
-git submodule update --remote
+git checkout promotion-js && git submodule update --remote
 git add promotion-js && git commit -m "updated promotion" && git push origin %branch%
 
 echo DONE
