60204d874d35d96c9f4797425bdaecb1bd738d89
Marcin Lasota <marcin.lasota@ttpsc.pl>
2018-06-13 12:59:18 +0200

    changed push to gh-pages branch

--- updateRepositores.bat
+++ updateRepositores.bat
@@ -1,10 +1,10 @@
-git add -A && git commit -m "new promotion" && git push origin master
+git add -A && git commit -m "new promotion" && git push origin gh-pages
 cd ../jaki-jezyk-programowania || pause && exit
 git submodule update --remote
-git add promotion-js && git commit -m "updated promotion" && git push origin master
-cd ../devcave || pause && exit
+git add promotion-js && git commit -m "updated promotion" && git push origin gh-pages
+cd ../devcave || echo "Devcave won't be updated" && pause && exit
 git submodule update --remote
-git add promotion-js && git commit -m "updated promotion" && git push origin master
+git add promotion-js && git commit -m "updated promotion" && git push origin gh-pages
 
 echo DONE
 pause
