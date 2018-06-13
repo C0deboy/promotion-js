38d2bd859e774a31d4330370884d0bbb8431efee
C0deboy <ellucky4@gmail.com>
2018-06-13 19:47:31 +0200

    new promotion

--- updateRepositores.bat
+++ updateRepositores.bat
@@ -1,10 +1,24 @@
-git add -A && git commit -m "new promotion" && git push origin gh-pages
+@echo off
+
+:Ask
+set /P INPUT=Commit to gh-pages?[y/n]: %=%
+If /I "%INPUT%"=="y" set branch="gh-pages" & goto Execute
+If /I "%INPUT%"=="n" set branch="master" & goto Execute
+echo Incorrect input & goto Ask
+
+:Execute
+echo %branch%
+
+@echo on
+
+git add -A && git commit -m "new promotion" && git push origin %branch%
 cd ../jaki-jezyk-programowania || pause && exit
 git submodule update --remote
-git add promotion-js && git commit -m "updated promotion" && git push origin gh-pages
+git add promotion-js && git commit -m "updated promotion" && git push origin %branch%
 cd ../devcave || echo "Devcave won't be updated" && pause && exit
 git submodule update --remote
-git add promotion-js && git commit -m "updated promotion" && git push origin gh-pages
+git add promotion-js && git commit -m "updated promotion" && git push origin %branch%
 
 echo DONE
 pause
+exit
