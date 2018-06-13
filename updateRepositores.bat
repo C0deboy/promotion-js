1038821e25e394b9dba6db6ff4ae591815d1bae3
C0deboy <ellucky4@gmail.com>
2018-06-13 20:16:21 +0200

    new promotion

--- updateRepositores.bat
+++ updateRepositores.bat
@@ -8,11 +8,19 @@ echo Incorrect input & goto Ask
 
 :Execute
 
+echo UPDATING PROMOTION-JS\n
+
 git add -A && git commit -m "new promotion" && git push origin %branch%
 cd ../jaki-jezyk-programowania || pause && exit
+
+echo UPDATING JAKI-JEZYK-PROGRAMOWANIA\n
+
 git checkout promotion-js && git submodule update --remote
 git add promotion-js && git commit -m "updated promotion" && git push origin %branch%
 cd ../devcave || echo "Devcave won't be updated" && pause && exit
+
+echo UPDATING DEVCAVE\n
+
 git checkout promotion-js && git submodule update --remote
 git add promotion-js && git commit -m "updated promotion" && git push origin %branch%
 
